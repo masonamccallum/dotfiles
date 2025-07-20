c900b5bd6c183fa926d5dfe9e194554fd0daca6a
masonamccallum <masonamccallum@gmail.com>
2025-07-20 13:21:48 -0500

    startup script

---
+++ spack/initdevenv.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/spack/spack.git ~/spack
+cd ~/spack
+. share/spack/setup-env.sh
+cd ~
+spack env create dev-env dotfiles/spack/dev-env/spack.yaml
+spack env activate -p dev-env
+spack concretize
+spack install
